b254fbbf08ea1e90b8c203ccd3270b060901091a
osboxes.org <aldsan_512@yahoo.com>
2016-11-15 01:44:40 +0000

    debugging condvar test

--- threads/synch.c
+++ threads/synch.c
@@ -333,6 +333,20 @@ cond_init (struct condition *cond)
   list_init (&cond->waiters);
 }
 
+bool cond_priority_func (const struct list_elem *a,
+                             const struct list_elem *b,
+                             void *aux)
+{
+  struct semaphore aSema = list_entry(a, struct semaphore_elem, elem)->semaphore;
+  struct semaphore bSema = list_entry(b, struct semaphore_elem, elem)->semaphore;
+  if(list_empty(&bSema.waiters)) return true;
+  if(list_empty(&aSema.waiters)) return false;
+
+  struct thread* threadA = list_entry(list_front(&aSema.waiters), struct thread, elem);
+  struct thread* threadB = list_entry(list_front(&bSema.waiters), struct thread, elem);
+  return threadA->priority > threadB->priority;
+}
+
 /* Atomically releases LOCK and waits for COND to be signaled by
    some other piece of code.  After COND is signaled, LOCK is
    reacquired before returning.  LOCK must be held before calling
@@ -365,7 +379,8 @@ cond_wait (struct condition *cond, struct lock *lock)
   
   sema_init (&waiter.semaphore, 0);
   //insert in some order???
-  list_push_back (&cond->waiters, &waiter.elem);
+  //list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered(&cond->waiters, &waiter.elem, &cond_priority_func, NULL);
     lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -386,9 +401,11 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
+  if (!list_empty (&cond->waiters)) {
+    list_sort(&cond->waiters, &cond_priority_func, NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
